cbc29d2521ac0e8ce05d1cf4c052bfab210d7529
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2024-06-11 17:03:53 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DeamonAlex for DelPerroSands'
+description 'A script to monitor and log the performance of server resources.'
+version '1.0.0'
+
+server_script {
+    'config.lua',
+    'server.lua'
+}
